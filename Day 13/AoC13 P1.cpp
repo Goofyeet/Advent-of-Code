35108fc62d82acf1616e54cd09c236c198a18c96
Austin <austinputland6464@gmail.com>
2023-02-16 11:08:16 -0400

    Add day 13 files

---
+++ Day 13/AoC13 P1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <deque>
+
+std::ifstream input("input2.txt");
+std::string line;
+
+    while (getline(input, line))
+    {
+
+
+    }
+
+int main()
+{
+
+    std::cout << "Fewest Steps possible: " << distance << std::endl;
+
+    return 0;
+}
