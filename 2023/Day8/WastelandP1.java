632b1dc7f2beb0b01821e278006b580210758ee4
Austin <austinputland6464@gmail.com>
2023-12-20 12:09:33 -0400

    Finish day 8 part 1!

--- 2023/Day8/WastelandP1.java
+++ 2023/Day8/WastelandP1.java
@@ -4,148 +4,63 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class WastelandP1 {
     public static void main(String args[]){
-        String path = "2023/Day8/test.txt";
+        String path = "2023/Day8/input.txt";
         try{
             Scanner fileReader = new Scanner(new File(path));
-            int total = 0;
-            ArrayList<String[]> handList = new ArrayList<>();
+            long totalSteps = 0;
+            int lineNum = 0;
+            Hashtable<String, String[]> adjList = new Hashtable<>();
             String line = "";
+            String currNode = "AAA";
+            String directions = fileReader.nextLine();
+            fileReader.nextLine();
 
+            //build adjacency list
             while(fileReader.hasNextLine()){
                 line = fileReader.nextLine();
-                String[] strArr = line.split("\\s+");
 
-                handList.add(strArr);
+                String[] strArr = line.split("\\s+=\\s+");
+                String node = strArr[0];
+
+                String[] connected = strArr[1].substring(1,strArr[1].length() - 1).split(",\\s+");
+
+                adjList.put(node, connected);
             }
 
-            Comparator<String[]> compareHands = (String[] hand1, String[] hand2)-> {
-                int first = classifyHand(hand1[0]);
-                int next = classifyHand(hand2[0]);
+            boolean foundEnd = false;
+            int loopCount = 0;
+            int length = directions.length();
 
-                if(first > next){
-                    return 1;
+            while(!foundEnd){
+                for(int i = 0; i < length; i++){
+                    if(currNode.equals("ZZZ")){
+                        foundEnd = true;
+                        break;
+                    }
+
+                    char c = directions.charAt(i);
+                    int dirIndex = 0;
+
+                    if(c == 'R'){
+                        dirIndex = 1;
+                    }
+
+                    currNode = adjList.get(currNode)[dirIndex];
+                    totalSteps++;
                 }
-                else if(first < next){
-                    return -1;
-                }
-                else{
-                    return evalSameHand(hand1[0], hand2[0]);
-                }
-
-            };
-
-            handList.sort(compareHands);
-
-            for(int i = 0; i < handList.size(); i++){
-                int product = (i + 1) * Integer.parseInt(handList.get(i)[1]);
-
-                total += product;
+                //loopCount++;
+                //System.out.print("Loop count: " + loopCount + "   total steps: " + totalSteps);
             }
 
-
-            System.out.println("Total product: " + total);
+            System.out.println("Total Steps: " + totalSteps);
 
         } catch(FileNotFoundException e){
             System.out.println("File not found");
         }
-    }
-
-    public static int classifyHand(String hand){
-        ArrayList<Character> distinctChars = new ArrayList<>();
-        ArrayList<Integer> numChars = new ArrayList<>();
-
-        //loop through chars of first hand
-        for(int i = 0; i < hand.length(); i++){
-            char c = hand.charAt(i);
-
-            int charIndex = distinctChars.indexOf(c);
-
-            if(charIndex == -1){
-                distinctChars.add(c);
-                numChars.add(1);
-            }
-            else {
-                numChars.set(charIndex,numChars.get(charIndex) + 1);
-            }
-        }
-
-        int numPairs = 0;
-        int numTriple = 0;
-        int handNum = 1;
-        for(Integer num : numChars){
-            switch(num){
-                case 1:
-                    break;
-                case 2:
-                    numPairs++;
-                    break;
-                case 3:
-                    numTriple++;
-                    break;
-                default:
-                    handNum = num + 2;
-            }
-            if(handNum > 5){
-                break;
-            }
-        }
-        if(numTriple == 1){
-            if(numPairs == 1){
-                handNum = 5;
-            }
-            else{
-                handNum = 4;
-            }
-        }
-        else{
-            handNum += numPairs;
-        }
-
-        return handNum;
-    }
-
-    public static int evalSameHand(String hand1, String hand2){
-        char[][] letters = {{'A', '5'}, {'K', '4'}, {'Q', '3'}, {'J', '2'}, {'T', '1'}};
-        char[] chars = new char[2];
-
-        //hands are the same
-        for(int i = 0; i < 5; i++){
-            chars[0] = hand1.charAt(i);
-            chars[1] = hand2.charAt(i);
-
-            //at least one of them is a digit
-            if(Character.isDigit(chars[0]) || Character.isDigit(chars[1])){
-                if(chars[0] < chars[1]){
-                    return -1;
-                }
-                else if(chars[0] > chars[1]){
-                    return 1;
-                }
-            }
-            //both are letters
-            else{
-                for(int j = 0; j < letters.length; j++){
-                    if(chars[0] == letters[j][0]){
-                        chars[0] = letters[j][1];
-                    }
-                    if(chars[1] == letters[j][0]){
-                        chars[1] = letters[j][1];
-                    }
-                }
-
-                if(chars[0] < chars[1]){
-                    return -1;
-                }
-                else if(chars[0] > chars[1]){
-                    return 1;
-                }
-            }
-        }
-
-        return 0;
     }
 }
